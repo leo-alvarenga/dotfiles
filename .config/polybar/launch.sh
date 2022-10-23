8eab6c7f106f4b2cdf5a71c8269bc89963803806
Leonardo A. Alvarenga <leosgalvarenga@protonmail.com>
2022-10-23 03:17:48 -0300

    Further customizations; New polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,5 +4,6 @@ polybar-msg cmd quit
 
 echo "---" | tee -a /tmp/polybar1.log
 polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar bottom 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bar launched"
