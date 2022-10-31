e9a7fc42db8cb8116dbe0fc9dff7a1381e135b27
Leonardo A. Alvarenga <leosgalvarenga@protonmail.com>
2022-10-31 01:07:02 -0300

    New i3lock scritp

--- .config/i3lock/i3lock.sh
+++ .config/i3lock/i3lock.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-LOCK_IMAGE_PATH="$HOME/Pictures/wallpapers/win10_funny.png"
-RESIZED_IMAGE_PATH="$HOME/Pictures/wallpapers/lock_resize_auto.png"
+LOCK_IMAGE_PATH="$HOME/Pictures/wallpapers/i3lock_current_screen.png"
+RESIZED_IMAGE_PATH="$HOME/Pictures/wallpapers/i3lock_current_screen_blurred.png"
 
 # greps screen info, filter to get dimension and cut off everything but the actual value
 RES="$(xdpyinfo | grep dimensions | cut -d' ' -f7)"
 
+# blur strength
+# use less than 20 tho, any values greater than that tends
+# to cause the blur algorithm to take more than .3s to execute
+BLUR="0x5"
+
+scrot --file "$LOCK_IMAGE_PATH"
+
+if [[ $(cat "$LOCK_IMAGE_PATH") > /dev/null  ]]; then
+	rm "$LOCK_IMAGE_PATH"
+fi
 
 convert -scale "$RES" "$LOCK_IMAGE_PATH" "$RESIZED_IMAGE_PATH"
+convert -blur "$BLUR" "$RESIZED_IMAGE_PATH" "$RESIZED_IMAGE_PATH"
 
-# locks screen, enables pointer, failed attempts count and sets wallpaper
+# locks screen, enables mouse pointer, enables failed attempts count and sets wallpaper
 i3lock --pointer default --image "$RESIZED_IMAGE_PATH" --show-failed-attempts
 
 # actually suspends the machine
