517c05361f1cb8c39abc67ef9634ef630059b372
Leonardo A Alvarenga <leosgalvarenga@protonmail.com>
2025-08-23 13:57:25 -0300

    feat: add menu widget and fix small issues

---
+++ scripts/launcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MENU=fuzzel
+MENU_PID=$(pidof $MENU)
+
+if [[ -z $MENU_PID ]]; then
+    exec $MENU
+    exit 0
+fi
+
+killall $MENU
---
+++ scripts/notifications.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function get_count() {
+    echo $(swaync-client -c)
+}
+
+function main() {
+    case $1 in
+        count)
+            get_count
+        ;;
+        *)
+            exit 0
+        ;;
+    esac
+}
+
+main $1
