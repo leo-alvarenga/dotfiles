09cc7d79308ec3a41201dea4693a2ad4800c4e05
Leonardo A. Alvarenga <leonardo.a.alvarenga@gmail.com>
2025-11-06 15:51:37 -0300

    feat: add picker and adjust configs

--- nvim/lua/config/workspaces.lua
+++ nvim/lua/config/workspaces.lua
@@ -3,24 +3,33 @@ local utils = require("config.utils")
 local workspaces = {}
 
 function workspaces.manage_workspaces()
+	local mini_pick = require("mini.pick")
+
 	local opts = {
-		"Add current directory as workspace",
-		"Remove current directory from workspaces",
+		"Add to workspaces",
+		"Remove from workspaces",
+		"Add all sub-directories to workspaces",
+		"Remove all sub-directories from workspaces",
+		"Sync",
 	}
 
-	vim.ui.select(opts, { prompt = "Manage Workspaces" }, function(choice)
-		if choice == opts[1] then
-			vim.cmd("WorkspacesAdd")
+	local actions = {
+		[opts[1]] = "WorkspacesAdd",
+		[opts[2]] = "WorkspacesRemove",
+		[opts[3]] = "WorkspacesAddDir",
+		[opts[4]] = "WorkspacesRemoveDir",
+		[opts[5]] = "WorkspacesSync",
+	}
 
-			return
+	mini_pick.ui_select(opts, { prompt = "Manage Workspaces" }, function(choice)
+		if actions[choice] ~= nil then
+			vim.cmd(actions[choice])
 		end
-
-		vim.cmd("WorkspacesRemove")
 	end)
 end
 
 function workspaces.on_open(_, path)
-	vim.cmd("cd " .. path .. ' | ' .. utils.constants.telescope.find_files_cmd)
+	vim.cmd("cd " .. path .. " | " .. utils.constants.telescope.find_files_cmd)
 end
 
 return workspaces
